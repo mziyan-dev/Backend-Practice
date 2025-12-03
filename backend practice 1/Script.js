66a582dc88f8cc37488f2d7270e5a3744d340998
mziyan-dev <m.ziyan@paz.agency>
2025-12-03 18:40:29 +0500

    first commit

---
+++ backend practice 1/Script.js
@@ -0,0 +1,82 @@
+// Arrys ///
+// ForEach 
+//  map return new arry 
+// OBJECTS // 
+// FUNCTION & RETURN // 
+// ASYNC FUNCTION ///
+
+
+let arr = [1, 2, 3, 4]
+
+//  ForEach /// 
+
+// arr.forEach((val)=>{
+//   console.log(val + " hello");
+// })
+
+
+
+//  Map ///
+
+// let ans  = arr.map(function(){
+// return 13;
+// })   
+// console.log(ans);
+
+
+// let filterdVal = arr.filter((val)=>{
+//   if (val >= 2) {
+//     return true
+//   }
+// })
+// console.log(filterdVal);
+
+
+// let findArr = arr.find((val)=>{
+//   if (val === 3){
+//     return val
+//   }
+// })
+// console.log(findArr)
+
+
+
+//  -----------------------------------------------ARRYS 4 METHOD COMPLETE  --------------------------------------------------------------//
+
+
+//  -----------------------------------------------OBJECT STARTED  -----------------------------------------------------------------------//
+
+
+// var obj = {
+//     name: "hello",
+//     age: 334,
+//     profeesion: "nothing"
+// }
+
+// obj.age = 123
+
+//  -----------------------------------------------OBJECT ENDED  -----------------------------------------------------------------------//
+
+//  -----------------------------------------------FUNCTION & RETURN STARTED-------------------------------------------------------------//
+
+// function abc() {
+//     return 12;
+// }
+
+// let ans = abc()
+
+//  -----------------------------------------------FUNCTION & RETURN ENDED----------------------------------------------------------------//
+
+//  -----------------------------------------------ASYNC FUNCTION STARTED----------------------------------------------------------------//
+
+
+// async function hello() {
+//     var blob = await fetch('https://randomuser.me/api/')
+//     var ans = await blob.json();
+//     console.log(ans);
+
+// }
+
+
+
+//  -----------------------------------------------ASYNC FUNCTION ENDED----------------------------------------------------------------//
