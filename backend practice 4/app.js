1f45eab80e4ee3c7ee841ae5c54208c35a76b4c8
mziyan-dev <m.ziyan@paz.agency>
2025-12-09 17:52:23 +0500

    first commit

--- backend practice 4/app.js
+++ backend practice 4/app.js
@@ -4,12 +4,19 @@
 // Setting up public static files//
 
 
-import express from 'express'
+
+
+import express from "express";
+import Path from "path"
+
+// const Path = require('path')
 const app = express()
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(Path.join(__dirname,"public")))
 app.set("view engine",'ejs')
 
 app.get("/", (req, res) => {
@@ -25,3 +32,31 @@ app.listen(3000,()=>{
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/profile/:username", (req, res) => {
+    // req.params.username
+    // res.render("index")
+// })
+
+
+// app.get("/profile/:username/:age", (req, res) => {
+    // req.params.username
+    // res.render("index")
+// })
