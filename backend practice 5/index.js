c97fd03189205171912ea21dccf35e68e522d294
mziyan-dev <m.ziyan@paz.agency>
2025-12-15 18:00:13 +0500

    some changes in file

--- backend practice 5/index.js
+++ backend practice 5/index.js
@@ -20,6 +20,18 @@ app.get("/files/:filename", (req, res) => {
     res.render("show",{filename:req.params.filename,filedata:filedata});
   })
 });
+
+app.get("/edit/:filename", (req, res) => {
+    res.render("edit",{filename: req.params.filename});
+ 
+});
+
+app.post("/edit", (req, res) => {
+  fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+  res.redirect("/")
+  })
+});
+
 app.post("/create", (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.detail, (err) => {
         res.redirect("/");
