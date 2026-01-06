903cfce0095fa402400e038f40c4bf06f980f1f3
mziyan-dev <m.ziyan@paz.agency>
2026-01-06 18:17:00 +0500

    Enhance chess game functionality by implementing player roles, move validation, and board rendering; update index view for improved layout and styling.

--- Backend project 2/app.js
+++ Backend project 2/app.js
@@ -29,9 +29,47 @@ app.get('/', (req, res) => {
 });
 
 
-io.on("connection",(uniquesocket)=>{
+io.on("connection", (uniquesocket) => {
     console.log('connected');
-})
+
+    if (!players.white) {
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+
+    } else if (!players.black) {
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+
+    } else {
+        uniquesocket.emit("spectatorRole");
+    }
+    uniquesocket.on("disconnect", function () {
+        if (uniquesocket.id === players.white) {
+            delete players.white;
+        } else if (uniquesocket.id === players.black) {
+            delete players.black;
+        }
+    })
+    uniquesocket.on("move", function (move) {
+        try {
+            if (chess.turn === "w" && uniquesocket.id !== players.white) return;
+            if (chess.turn === "b" && uniquesocket.id !== players.black) return;
+            const result = chess.move(move);
+            if (result) {
+                currentplayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+
+            } else {
+                console.log("invalid move : ", move);
+                uniquesocket.emit("invalid move : ", move);
+            }
+        } catch (err) { 
+            console.log(err);
+            uniquesocket.emit("invalid move : ", move);
+        }
+    })
+});
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
